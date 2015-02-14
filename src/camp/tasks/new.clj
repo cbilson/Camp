84bdd77fdaaf337f3967be271e320d30d300bd96
Chris Bilson <cbilson@pobox.com>
2015-02-14 14:11:59 -0800

    thought I could get away with pprint for code
    Looks too ugly. Switching back to something simpler.

--- src/camp/tasks/new.clj
+++ src/camp/tasks/new.clj
@@ -1,6 +1,38 @@
+;; -*- compilation-command: "msbuild /t:CampExe /verbosity:minimal"
 (ns camp.tasks.new
-  (:require [camp.core :refer [getenv]]))
+  (:require [clojure.clr.io :as io]
+            [clojure.pprint :refer [pprint with-pprint-dispatch code-dispatch]]
+            [camp.core :refer [getenv]]
+            [camp.io :as cio]))
+
+(defn pprint-form [form]
+  (with-pprint-dispatch code-dispatch
+    (pprint form)))
+
+(defn gitignore []
+  (println "/targets")
+  (println "/checkouts")
+  (println "/packages")
+  (println "*.dll")
+  (println "/src/**/*.exe")
+  (println "/.nrepl-port")
+  (println "/.repl")
+  (println "/out"))
+
+(defn project-clj [name]
+  (pprint-form
+   `(defproject ~name "0.1.0-SNAPSHOT"
+      :depends [[Clojure "1.6.0.1"]]
+      :plugins [['not-supported-yet]])))
+
+(defn write-template [fname template-fn & args]
+  (with-open [writer (io/text-writer fname)]
+    (binding [*out* writer]
+      (apply template-fn args))))
 
 (defn new
   "Create a new ClojureCLR project from a template."
-  [project & args])
+  [{:keys [name] :as project} & args]
+  (cio/mkdir name)
+  (write-template (cio/file (str name) "project.clj") project-clj name)
+  (write-template (cio/file (str name) ".gitignore") gitignore))
