14855b4cc488cc03e53cee1f54c2c0ffd2de8ae1
Chris Bilson <cbilson@pobox.com>
2015-02-19 07:01:13 -0800

    compile compiles but doesn't save any binaries

--- src/camp/tasks/compile.clj
+++ src/camp/tasks/compile.clj
@@ -1,4 +1,14 @@
 (ns camp.tasks.compile
-  (:refer-clojure :exclude [compile]))
+  (:refer-clojure :exclude [compile])
+  (:require [camp.io :as io] 
+            [camp.tasks.deps :as deps]))
 
-(defn compile [project & _])
+(defn compile
+  "Compile the project into assemblies and exes."
+  [{:keys [source-paths] :as proj} & _]
+  (doseq [source-path source-paths]
+    (binding [clojure.core/*compile-path* source-path]
+      (println "*compile-path:" *compile-path*)
+      (doseq [source-file (io/files source-path "*.clj" :AllDirectories)]
+        (println "compiling" source-file)
+        (compile source-file)))))
