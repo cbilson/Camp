3368ddb8a227dc0e3aef55f73ba5bc4bae12c01b
Chris Bilson <cbilson@pobox.com>
2015-03-13 05:20:30 -0700

    add template for web application

--- src/camp/project_template.clj
+++ src/camp/project_template.clj
@@ -3,6 +3,22 @@
   (:require [camp.io :as cio]
             [camp.templates :as t]))
 
+(defn webapp
+  "Create a web application project that uses webapi."
+  [{:keys [name safe-name] :as project} & _]
+  (let [model (t/project->model project)
+        asm (assembly-load "camp.resources")
+        render (fn [t f]
+                 (t/render-template-to-file model asm (str "templates." t) f))]
+    (cio/mkdir name)
+    (render "webapp.project.clj" (cio/file name "project.clj"))
+    (render "gitignore" (cio/file name ".gitignore"))
+    (cio/mkdir (cio/file name "src"))
+    (cio/mkdir (cio/file name "src" safe-name))
+    (render "webapp.core.clj" (cio/file name "src" safe-name "core.clj"))
+    
+    (println "Created a new ClojureCLR webapp camp project named" name ".")))
+
 (defn default
   "Create a default project, which has a core.clj and produces an exe."
   [{:keys [name safe-name] :as project} & _]
--- src/camp/tasks/new.clj
+++ src/camp/tasks/new.clj
@@ -4,7 +4,7 @@
             [camp.project :as cp]
             [camp.templates :as templates]))
 
-(defn resolve-template-fn
+(defn- resolve-template-fn
   "Templates will be functions in the camp.project-templates namespace."
   [name]
   (let [ns (str "camp.project-template")
@@ -16,8 +16,9 @@
 
 (defn new
   "Create a new ClojureCLR project from a template."
-  [_ name & [project-template & rest]]
-  (let [safe-name (.Replace name "-" "_")
+  [_  & [template name & rest]]
+  (let [[template name] (if (nil? name) ["default" template] [template name])
+        safe-name (.Replace name "-" "_")
         new-project (assoc cp/project-defaults :name name :safe-name safe-name)
-        project-fn (resolve-template-fn (or project-template "default"))]
+        project-fn (resolve-template-fn template)]
     (apply project-fn new-project rest)))
