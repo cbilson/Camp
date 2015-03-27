bca58411d4a1d58ee46bf5b86420962e6f7f0e1a
Chris Bilson <cbilson@pobox.com>
2015-03-27 10:41:18 -0700

    cleanup clean task

--- src/camp/tasks/clean.clj
+++ src/camp/tasks/clean.clj
@@ -1,15 +1,28 @@
 (ns camp.tasks.clean
   "Task to clean up any generated artifacts in a camp project."
-  (:require [camp.core :refer [info]]
-            [camp.io :as io]))
+  (:require [camp.core :refer [debug info]]
+            [camp.io :as io]
+            [camp.project :as proj]))
+
+(defn- remove-if-exists [proj maybe-relative-path]
+  (let [path (proj/resolve-relative-path proj maybe-relative-path)]
+    (debug "full path:" path)
+    (cond
+      (io/directory-exists? path)
+      (do
+        (info "Removing" path)
+        (io/remove-dir! path true))
+
+      (io/file-exists? path)
+      (do
+        (info "Removing" path)
+        (io/remove-file! path)))))
 
 (defn clean
   "Clean up an generated artifacts in a project."
   [project & options]
-  (when-let [dir (io/directory-exists? (:targets-path))]
-    (info "Removing" dir)
-    (io/remove-dir! dir))
+  (doseq [dir [(:targets-path project)]]
+    (remove-if-exists project dir))
   (when (some #{"--scorch"} options)
-    (when-let [dir (io/directory-exists? (:packages-path project))]
-      (info "Removing" dir)
-      (io/remove-dir! dir))))
+    (doseq [dir [(:packages-path project)]]
+      (remove-if-exists project dir))))
