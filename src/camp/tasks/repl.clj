6c7c575950b55d2f475ab9a65eed340e0d340915
Chris Bilson <cbilson@pobox.com>
2015-04-21 16:27:05 -0700

    fix deps in repl task

--- src/camp/tasks/repl.clj
+++ src/camp/tasks/repl.clj
@@ -4,7 +4,8 @@
             [camp.core :refer [debug]]
             [camp.main :as cm]
             [camp.io :as cio]
-            [camp.project :as cp]))
+            [camp.project :as cp]
+            [camp.nuget :as nuget]))
 
 (defn repl
   "Start a REPL. The REPL will have all the source code files
@@ -14,7 +15,7 @@
   (let [repl-args (remove cm/camp-options args)
         dir (cp/resolve-relative-path project (first source-paths))]
     (debug "Changing to src dir" dir)
-    (cp/with-assembly-resolution project
+    (cp/with-assembly-resolution (nuget/libs project)
       (cio/with-current-directory
         dir
         (apply clojure.main/main repl-args)))))
